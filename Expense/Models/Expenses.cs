62d5bb0ea50998f25f76bc832665cd2b3c6c995e
shinzuo <fantastiassasin@gmail.com>
2025-01-17 02:16:58 +0800

    Initialize

---
+++ Expense/Models/Expenses.cs
@@ -0,0 +1,11 @@
+﻿namespace Expense.Models
+{
+    public class Expenses
+    {
+        public int Id { get; set; }
+
+        public decimal Value { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
---
+++ Expense/Models/ExpensesDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense.Models
+{
+    public class ExpensesDBContext : DbContext
+    {
+        public DbSet<Expenses> Expenses { get; set; }
+
+        public ExpensesDBContext(DbContextOptions<ExpensesDBContext>options)
+            : base(options)
+        {
+            
+        }
+    }
+}
