32055d504c545b29b263026f888e20ce5f79963c
shinzuo <fantastiassasin@gmail.com>
2025-01-31 20:55:43 +0800

    Inventory uploaded

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using InventoryMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryMVC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
